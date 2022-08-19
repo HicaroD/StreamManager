8452c67bc52f33adb0ab7edb3468e2ed94dd0896
HicaroD <hdanrlley1@gmail.com>
2022-08-18 22:36:34 -0300

    proj: connecting to irc server with tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::*;
+use std::collections::HashMap;
+use std::net::{TcpListener, TcpStream};
+
+use anyhow::Result;
+//use reqwest;
+//use tokio;
+
+const TWITCH_IRC_ADDRESS: &str = "irc.twitch.tv:443";
+const BUFFER_SIZE: usize = 2048; 
+
+fn main() -> Result<()> {
+    let mut irc_connection = TcpStream::connect(TWITCH_IRC_ADDRESS)?;
+    let mut buffer = [0; BUFFER_SIZE];
+    loop {
+        irc_connection.read(&mut buffer)?;
+        let message = std::str::from_utf8(&buffer)?;
+        println!("{}", message);
+    }
 }
